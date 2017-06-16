387b6dac050276b6d6fb8a44819f8cce2dfb1f3a
Cris <Cris>
2017-06-16 19:13:24 -0400

    new edit test

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-#from bson import json_util
-#from pymongo import MongoClient
+from bson import json_util
+from pymongo import MongoClient
 
 
 MONGODATABASE = "my_db"
 MONGOSERVER = "localhost"
 MONGOPORT = 27017
-#client = MongoClient(MONGOSERVER, MONGOPORT)
-#mongodb = client[MONGODATABASE]
+client = MongoClient(MONGOSERVER, MONGOPORT)
+mongodb = client[MONGODATABASE]
 
 if __name__ == "__main__":
-    query = "12312312"
-    results = "escuchas.find({{'$text':{{'$search':'{}'}},{{'_id':1,'contenido':1,'fecha':1,'numero':1,'ciudad':1}})".format(query)
-    #results = eval('mongodb.' + query)
-    #results = json_util.dumps(results, sort_keys=True, indent=4)
+    query = input()
+    #results = "escuchas.find({{'$text':{{'$search':'{}'}},{{'_id':1,'contenido':1,'fecha':1,'numero':1,'ciudad':1}})".format(query)
+    results = eval('mongodb.' + query)
+    results = json_util.dumps(results, sort_keys=True, indent=4)
     print(results)
