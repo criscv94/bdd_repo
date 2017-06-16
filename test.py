52979ebb66f9c7151fa4642c582f281ad5396b7f
Cris <Cris>
2017-06-15 22:52:19 -0400

    testing file for mongo

---
+++ test.py
@@ -0,0 +1,13 @@
+from bson import json_util
+from pymongo import MongoClient
+
+MONGODATABASE = "my_db"
+MONGOSERVER = "localhost"
+MONGOPORT = 27017
+client = MongoClient(MONGOSERVER, MONGOPORT)
+mongodb = client[MONGODATABASE]
+
+query = input()
+results = eval('mongodb.' + query)
+results = json_util.dumps(results, sort_keys=True, indent=4)
+print(results)
