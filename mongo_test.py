1550c27f4a8070ead975b97adaf657396849d7aa
Cris <Cris>
2017-06-17 13:43:07 -0400

    edit test files

---
+++ mongo_test.py
@@ -0,0 +1,17 @@
+from bson import json_util
+from pymongo import MongoClient
+
+
+MONGODATABASE = "my_db"
+MONGOSERVER = "localhost"
+MONGOPORT = 27017
+client = MongoClient(MONGOSERVER, MONGOPORT)
+mongodb = client[MONGODATABASE]
+
+if __name__ == "__main__":
+    query = input("Ingrese el query:\n")
+    query = query.replace("%20", " ")
+    results = "escuchas.find({{'$text':{{'$search':'\\'{}\\\''}}}})".format(query)
+    results = eval('mongodb.' + query)
+    results = json_util.dumps(results, sort_keys=True, indent=4)
+    print(results)
--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-#from bson import json_util
-#from pymongo import MongoClient
 
-
-MONGODATABASE = "my_db"
-MONGOSERVER = "localhost"
-MONGOPORT = 27017
-#client = MongoClient(MONGOSERVER, MONGOPORT)
-#mongodb = client[MONGODATABASE]
 
 if __name__ == "__main__":
-    query = "12312312"
+    query = input()
+    query = query.replace("%20", " ")
     results = "escuchas.find({{'$text':{{'$search':'\\'{}\\\''}}}})".format(query)
-    #results = eval('mongodb.' + query)
-    #results = json_util.dumps(results, sort_keys=True, indent=4)
     print(results)
